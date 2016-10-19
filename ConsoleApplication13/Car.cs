45d4c1774cc3e70fbac7bf9c280bbcea0a86dd2b
MrMaryjusz <mdrzewoski@gmail.com>
2016-10-19 14:40:00 +0200

    Ćwiczenie nr 3

---
+++ ConsoleApplication13/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+
+        public Car()
+        {
+            pojemnoscSilnika = 4.8;
+            marka = "Skoda";
+        }
+        public Car(double pojemnoscSilnika, string marka)
+        {
+            this.pojemnoscSilnika = pojemnoscSilnika;
+            this.marka = marka;
+        }
+        static public Car Create()
+        {
+            return new Car();
+
+        }
+        static int iloscKol;
+        static Car()
+        {
+            iloscKol = 4;
+        }
+        ~Car()
+        {
+          Console.WriteLine("Zwalniam pamięć");
+          Console.ReadKey();
+        }
+
+    }
+}
---
+++ ConsoleApplication13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+
+            Car car2 = new Car(3.7, "Ford");
+
+            Car car3 = Car.Create();
+
+        }
+    }
+}
